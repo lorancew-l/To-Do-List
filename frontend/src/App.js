d011c7be8ce7f69ef1f090995db7e7b4eea36c46
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-06-08 17:55:24 +0500

    Added task on click handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,23 @@ import React, { Component, Fragment } from 'react';
 import './css/style.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar/SideBar'
+import Main from './components/Main/Main'
 
 export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {showModal: false}
+  }
+
+  showModal = () => this.setState({showModal: true})
+  closeModal = () => this.setState({showModal: false})
+
   render() {
     return (
       <Fragment>
         <Header></Header>
         <Sidebar></Sidebar>
+        <Main onTaskClick = {this.showModal}></Main>
       </Fragment>
     );
   };

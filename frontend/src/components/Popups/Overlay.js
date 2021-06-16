a0a68b2dd53916b35671ac0ebc49a812103e1b89
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-06-16 19:53:11 +0500

    Overlay reworked to functional component

--- frontend/src/components/Popups/Overlay.js
+++ frontend/src/components/Popups/Overlay.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+
 
 const modalRoot = document.getElementById('modal-root')
 
-export class Overlay extends Component {
-  render() {
-    return ReactDOM.createPortal(
-      <div className="popup-overlay" onClick={this.props.closePopup}>
-        {React.cloneElement(this.props.children, { pos: this.props.popupPos })}
-      </div>,
-    modalRoot)
-  }
+export default function Overlay(props) {
+  return ReactDOM.createPortal(
+    <div className="popup-overlay" onClick={props.closePopup}>
+      {React.cloneElement(props.children, { pos: props.popupPos })}
+    </div>,
+  modalRoot)
 }
-
-export default Overlay

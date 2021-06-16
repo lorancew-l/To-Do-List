c1eca81bb7a3a4a485b12985b6b493cd664945fc
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-06-16 19:11:49 +0500

    Sidebar component reworked to functional component

--- frontend/src/components/Sidebar/SideBar.js
+++ frontend/src/components/Sidebar/SideBar.js
@@ -1,49 +1,36 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { dailyTasksActive, clipboard, importantTaskActive } from '../../images/index'
 import SidebarAddItemButton from './SidebarAddItemButton'
 import { SidebarItem } from './SidebarItem'
 
 
-export class SideBar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {items: [{id: 0, title: 'Сегодня', icon: dailyTasksActive, style: 'sidebar-item item_selected'},
-                          {id: 1, title: 'Важно', icon: importantTaskActive, style: 'sidebar-item'},
-                          {id: 2, title: 'Задачи', icon: clipboard, style: 'sidebar-item'}]}
-  
-    this.itemOnClickHandler = this.itemOnClickHandler.bind(this)
+export default function SideBar(props) {
+  const [items, setItems] = useState([{id: 0, title: 'Сегодня', icon: dailyTasksActive, style: 'sidebar-item item_selected'},
+                                      {id: 1, title: 'Важно', icon: importantTaskActive, style: 'sidebar-item'},
+                                      {id: 2, title: 'Задачи', icon: clipboard, style: 'sidebar-item'}])
+
+  const itemOnClickHandler = (itemId) => {
+    setItems(items.map(element => {
+      element.style = (element.id === itemId)? 'sidebar-item selected': 'sidebar-item'
+      return element 
+    }))
   }
 
-  itemOnClickHandler(itemId) {
-    this.setState(prevState => {
-      return prevState.items.map(element => {
-        element.style = (element.id === itemId)? 'sidebar-item selected': 'sidebar-item'
-        return element 
-      })
-    })
-  }
+  const addItemOnClickHandler = () => {}
 
-  addItemOnClickHandler() {
-
-  }
-
-  render() {
-    return (
-      <aside className={this.props.showSidebar? 'sidebar on' : 'sidebar off'}>
-        <div className='sidebar-content'>
-          <ul>
-            {
-              this.state.items.map(item => {
-                return <SidebarItem key={item.id} id={item.id} title={item.title} icon={item.icon} className={item.style}
-                                    onClick={this.itemOnClickHandler}></SidebarItem>
-              })
-            }
-            <SidebarAddItemButton onClick={this.addItemOnClickHandler}></SidebarAddItemButton>
-          </ul>
-        </div>
-      </aside>
-    )
-  }
+  return (
+    <aside className={props.showSidebar? 'sidebar on' : 'sidebar off'}>
+    <div className='sidebar-content'>
+      <ul>
+        {
+          items.map(item => {
+            return <SidebarItem key={item.id} id={item.id} title={item.title} icon={item.icon} className={item.style}
+                                onClick={itemOnClickHandler}></SidebarItem>
+          })
+        }
+        <SidebarAddItemButton onClick={addItemOnClickHandler}></SidebarAddItemButton>
+      </ul>
+    </div>
+  </aside>
+  )
 }
-
-export default SideBar

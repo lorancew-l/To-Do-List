7874a562c2175c671a398e5ca2336d1ed1ddb237
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-06-16 19:13:16 +0500

    SidebarAddItemButton reworked to functional component

--- frontend/src/components/Sidebar/SidebarAddItemButton.js
+++ frontend/src/components/Sidebar/SidebarAddItemButton.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { addTask } from '../../images/index'
 
-
-export class SidebarAddItemButton extends Component {
-  render() {
-    return (
-      <li className='sidebar-item' onClick={this.props.onClick}>
-        <img alt='Создать список' src={addTask}/>
-        <span className="sidebar-item-text">Создать список</span>
-      </li>
-    )
-  }
+export default function SidebarAddItemButton(props) {
+  return (
+    <li className='sidebar-item' onClick={props.onClick}>
+      <img alt='Создать список' src={addTask}/>
+      <span className="sidebar-item-text">Создать список</span>
+    </li>
+  )
 }
-
-export default SidebarAddItemButton

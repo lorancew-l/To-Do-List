beb67d28814315bfa299ad930374af1d99a989e7
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-06-16 19:13:03 +0500

    Header reworked to functional component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { openSideBar, search, quickTask, settings } from '../images/index'
 
 
-export class Heder extends Component {
-  render() {
-    return (
-      <header className="header">
-        <div className="header-inner">
-          <div className="header-left">
-            <button className="" onClick={this.props.onSidebarChange}>
-              <img className="" alt='open sidebar' src={openSideBar}/>
-            </button>
+export default function Header(props) {
+  return (
+    <header className="header">
+    <div className="header-inner">
+      <div className="header-left">
+        <button className="" onClick={props.onSidebarChange}>
+          <img className="" alt='open sidebar' src={openSideBar}/>
+        </button>
 
-            <div className="header-search">
-              <img className="" alt='search' src={search}/>
-              <input type="text" placeholder="Поиск"/>
-            </div>
-          </div>
-
-          <div className="header-right">
-            <button className="">
-              <img className="" alt='add task' src={quickTask}/>
-            </button>
-            <button className="">
-              <img className="" alt='settings' src={settings}/>
-            </button>
-          </div>
+        <div className="header-search">
+          <img className="" alt='search' src={search}/>
+          <input type="text" placeholder="Поиск"/>
         </div>
-      </header>
-    )
-  }
-}
+      </div>
 
-export default Heder
+      <div className="header-right">
+        <button className="">
+          <img className="" alt='add task' src={quickTask}/>
+        </button>
+        <button className="">
+          <img className="" alt='settings' src={settings}/>
+        </button>
+      </div>
+    </div>
+  </header>
+  )
+}

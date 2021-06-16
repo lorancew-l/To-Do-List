969cb4536bd01e77cbb53b045e4821b9cde6b4f6
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-06-16 19:54:29 +0500

    AddTaskForm reworked to functional component

--- frontend/src/components/Main/AddTaskForm/AddTaskForm.js
+++ frontend/src/components/Main/AddTaskForm/AddTaskForm.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import NoFocusContent from './NoFocusContent'
 import OnFocusContent from './OnFocusContent'
 
-export class AddTask extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {onFocus: false}
-  }
 
-  addTaskClickHandler = () => this.setState({onFocus: true})
-  cancelClickHandler = () => {
-    this.setState({onFocus: false})
-  }
-  submitClickHandler = () => {}
-  
-  render() {
-    return (
-      <form>
-        {this.state.onFocus? <OnFocusContent onCancelClick={this.cancelClickHandler} showPopup={this.props.showPopup}
-                                             updatePopupPos={this.props.updatePopupPos}></OnFocusContent> :
-                             <NoFocusContent onClick={this.addTaskClickHandler}></NoFocusContent>}
-      </form>
-    )
-  }
+export default function AddTaskForm(props) {
+  const [onFocus, setOnFocus] = useState(false)
+
+  function addTaskClickHandler() {setOnFocus(true)}
+
+  function cancelClickHandler() {setOnFocus(false)}
+
+  function submitClickHandler() {}
+
+  return (
+    <form>
+      {onFocus? <OnFocusContent onCancelClick={cancelClickHandler} showPopup={props.showPopup}
+                                updatePopupPos={props.updatePopupPos}></OnFocusContent>
+              : <NoFocusContent onClick={addTaskClickHandler}></NoFocusContent>}
+    </form>
+  )
 }
-
-export default AddTask

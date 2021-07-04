5df745b166fe26e0b270fb3823e54a28a0a10e1f
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-04 15:35:48 +0500

    added popup close prop

--- frontend/src/components/Popups/Overlay.js
+++ frontend/src/components/Popups/Overlay.js
@@ -7,7 +7,7 @@ const modalRoot = document.getElementById('modal-root')
 export default function Overlay(props) {
   return ReactDOM.createPortal(
     <div className="popup-overlay" onClick={props.closePopup}>
-      {React.cloneElement(props.children, { pos: props.popupPos })}
+      {React.cloneElement(props.children, { pos: props.popupPos, closePopup: props.closePopup })}
     </div>,
   modalRoot)
 }

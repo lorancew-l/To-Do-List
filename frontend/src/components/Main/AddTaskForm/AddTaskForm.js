715313d8486f3bf313aea9cf347ece11773be6b2
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-04 13:23:09 +0500

    added AddTaskForm submit handler

--- frontend/src/components/Main/AddTaskForm/AddTaskForm.js
+++ frontend/src/components/Main/AddTaskForm/AddTaskForm.js
@@ -2,20 +2,39 @@ import React, { useState } from 'react'
 import NoFocusContent from './NoFocusContent'
 import OnFocusContent from './OnFocusContent'
 
+import { addTask } from '../../../tools/api'
+
 
 export default function AddTaskForm(props) {
   const [onFocus, setOnFocus] = useState(false)
+  const [taskName, setTaskName] = useState('')
+  const [deadline, setDeadline] = useState(null)
+  const [deadlineStringRepresentation, setDeadlineStringRepresentation] = useState(null)
 
   function addTaskClickHandler() {setOnFocus(true)}
 
-  function cancelClickHandler() {setOnFocus(false)}
+  function cancelClickHandler() {
+    setOnFocus(false)
+    setTaskName('')
+    setDeadline(null)
+    setDeadlineStringRepresentation(null)
+  }
 
-  function submitClickHandler() {}
+  function onSubmitHandler(event) {
+    event.preventDefault()
+    console.log(addTask({'title': taskName, 'deadline': deadline}))
+  }
+
+  function deadlineChangeHandler(newDate, stringRepresentation) {
+    setDeadline(newDate)
+    setDeadlineStringRepresentation(stringRepresentation)
+  }
 
   return (
-    <form>
-      {onFocus? <OnFocusContent onCancelClick={cancelClickHandler} showPopup={props.showPopup}
-                                updatePopupPos={props.updatePopupPos}></OnFocusContent>
+    <form onSubmit={onSubmitHandler}>
+      {onFocus? <OnFocusContent onCancelClick={cancelClickHandler} showPopup={props.showPopup} taskName={taskName}
+                                updatePopupPos={props.updatePopupPos} deadline={deadlineStringRepresentation}
+                                onDateClick={deadlineChangeHandler} setTaskName={setTaskName}></OnFocusContent>
               : <NoFocusContent onClick={addTaskClickHandler}></NoFocusContent>}
     </form>
   )

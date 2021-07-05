7f0804a8cc9eca892fd41fcec92b5abd42029859
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-05 20:27:54 +0500

    replace put by patch in TaskDetail View

--- api/views.py
+++ api/views.py
@@ -26,8 +26,8 @@ class TaskDetail(generics.GenericAPIView,
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)

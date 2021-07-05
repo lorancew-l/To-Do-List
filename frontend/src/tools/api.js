fcf335e51607891352482f9c834e7dfc82acfabd
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-05 19:23:50 +0500

    added complete task request

--- frontend/src/tools/api.js
+++ frontend/src/tools/api.js
@@ -20,3 +20,19 @@ export async function getTaskList() {
     return await response.json()
   }
 }
+
+export async function completeTask(taskId) {
+  console.log(taskId);
+  const response = await fetch(`http://localhost:8000/api/task-detail/${taskId}/`, {
+    method: 'PATCH',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'omit',
+    headers: {'Content-Type': 'application/json'},
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify({completed: true})
+  })
+
+  return response
+}

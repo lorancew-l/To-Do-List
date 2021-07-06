b28d14296352f3e66bb181950f8570ab45ead16c
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-06 14:52:16 +0500

    added  id autofield to TaskModel

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class TaskModel(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=64, default='')
     creation_date = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)

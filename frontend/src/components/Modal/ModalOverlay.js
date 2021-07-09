8dfefb892a70915cb916e0371b3164a98e8c5537
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-09 18:00:50 +0500

    static classname

--- frontend/src/components/Modal/ModalOverlay.js
+++ frontend/src/components/Modal/ModalOverlay.js
@@ -5,9 +5,8 @@ import ReactDOM from 'react-dom'
 const modalRoot = document.getElementById('modal-root')
 
 export default function ModalOverlay(props) {
-  const className = props.shadow ? "modal-overlay shadow" : "modal-overlay"
   return ReactDOM.createPortal(
-    <div className={className} onClick={props.closeModal}>
+    <div className="modal-overlay shadow" onClick={props.closeModal}>
       {React.cloneElement(props.children, { pos: props.modalPos, closeModal: props.closeModal })}
     </div>,
   modalRoot)

c5deffb261c18dd570ac9f12c6a7d2460e4e20ba
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-14 14:47:09 +0500

    updated Calendar height

--- frontend/src/components/Main/AddTaskForm/OnFocusContent.js
+++ frontend/src/components/Main/AddTaskForm/OnFocusContent.js
@@ -10,7 +10,7 @@ export default function OnFocusContent(props) {
 
   const isFirstRun = useRef(true);
 
-  const calendarHeight = 476 
+  const calendarHeight = 472
   const [isCalendarOpen, setCalendarOpen] = useState(false)
   const [calendarPos, setCalendarPos] = useState({x: 0, y: 0})
 

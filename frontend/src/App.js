d94aa6c498bdafc1afb854a49ed8f90f79398c13
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-26 20:12:50 +0500

    App reworked to functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import './css/style.css'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar/SideBar'
@@ -9,56 +9,24 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import { isRefreshTokenExpired } from './tools/account'
 
 
-export class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {showSidebar: window.innerWidth >= 992 ? true : false,
-                  isLoggedIn: !isRefreshTokenExpired()}
-  }
+export default function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(window.innerWidth >= 992 ? true : false)
+  const [isLoggedIn, setLoggedIn] = useState(!isRefreshTokenExpired())
 
-  showModal = (modalContent) => {
-    this.setState({modalContent: modalContent})
-  }
-
-  closeModal = () => this.setState({modalContent: null})
-
-  showPopper = (popperContent) => {
-    this.setState({popperContent: popperContent})
-  }
-
-  updatePopperPos = (pos) => {
-    this.setState({popperPos: pos})
-  }
-
-  closePopper = () => this.setState({popperContent: null})
-  
-  handleSidebarChange = () => {
-    this.setState(prevState => {
-      return {showSidebar: prevState.showSidebar? false: true}
-    })
-  }
-
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/">
-          {this.state.isLoggedIn ? 
-  
-              <Fragment>
-                <Header onSidebarChange={this.handleSidebarChange}></Header>
-                <Sidebar showSidebar={this.state.showSidebar}></Sidebar>
-                <Main showSidebar={this.state.showSidebar}/>
-              </Fragment>
-
-          : <Redirect to="/login/"/>
-          }
-        </Route>
-        <Route path="/login/" component={LoginForm}></Route>
-        <Route path="/signup/" component={SignupForm} />
-      </Switch>
-
-    );
-  };
-};
-
-export default App;
+  return (
+    <Switch>
+      <Route exact path="/">
+        {isLoggedIn ? 
+          <Fragment>
+            <Header onSidebarChange={() => setSidebarOpen(!isSidebarOpen)}></Header>
+            <Sidebar showSidebar={isSidebarOpen}></Sidebar>
+            <Main showSidebar={isSidebarOpen}/>
+          </Fragment>
+        : <Redirect to="/login/"/>
+        }
+      </Route>
+      <Route path="/login/" component={LoginForm}></Route>
+      <Route path="/signup/" component={SignupForm} />
+    </Switch>
+  )
+}

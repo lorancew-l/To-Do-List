44a84aeedd05f8337906809600cf1a76096dc171
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-07-31 12:14:53 +0500

    added adaptive design for AccountForm

--- frontend/src/components/Account/AccountForm.js
+++ frontend/src/components/Account/AccountForm.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function AccountForm(props) {
-  const requiredFieldEmptyError = <div className="required-field-empty-error">Это поле обязательно для заполнения</div>
   const [email, password] = [props.email, props.password]
   const disabled = email.requiredEmpty || password.requiredEmpty
 
@@ -12,12 +11,10 @@ export default function AccountForm(props) {
         <div className="account-form-field">
           <label htmlFor="email">Email</label>
           <input name="email" type="email" value={email.value} {...email.bind}></input>
-          {email.requiredEmpty && requiredFieldEmptyError}
         </div>
         <div className="account-form-field">
           <label htmlFor="password">Пароль</label>
           <input name="password" type="password" value={password.value} {...password.bind}></input>
-          {password.requiredEmpty && requiredFieldEmptyError}
         </div>
         <button disabled={disabled} className={disabled ? "account-form-submit disabled" : "account-form-submit"}
                 type="submit">{props.submitButtonLabel}</button>
--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -7,7 +7,7 @@ export default function Main(props) {
   const currentDate = new Date()
 
   return (
-    <main className={props.showSidebar? "sidebar-on" : "sidebar-off"}>
+    <main className={props.showSidebar? "main sidebar-on" : "main sidebar-off"}>
       <div className={props.showSidebar? "sidebar-overlay on" : "sidebar-overlay off"}></div>
       <div className="container">
         <div className="task-list-header">
@@ -17,10 +17,10 @@ export default function Main(props) {
           </div>
         </div>
         <ul className="task-list">
-            {props.taskList.map(task => {
-              return <TaskListItem key={task.id} taskData={task} updateTaskList={props.updateTaskList}></TaskListItem>})
-            }
-            <AddTask updateTaskList={props.updateTaskList}></AddTask>
+          {props.taskList.map(task => {
+            return <TaskListItem key={task.id} taskData={task} updateTaskList={props.updateTaskList}></TaskListItem>})
+          }
+          <AddTask updateTaskList={props.updateTaskList}></AddTask>
         </ul>
       </div>
     </main>

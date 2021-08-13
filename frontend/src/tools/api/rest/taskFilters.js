a4d3806bea7e3c9901a4ef666019abef3599f1c4
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-08-13 15:18:36 +0500

    added add/patch/delete requests

--- frontend/src/tools/api/rest/taskFilters.js
+++ frontend/src/tools/api/rest/taskFilters.js
@@ -9,3 +9,29 @@ export function getTaskFilterList() {
     headers: {'Content-Type': 'application/json', authorization: true},
   })
 }
+
+export function addTaskFilter(data) {
+  return makeRequest({
+    url: baseURL,
+    method: 'POST',
+    headers: {'Content-Type': 'application/json', authorization: true},
+    data
+  })
+}
+
+export function deleteTaskFilter(filterId) {
+  return makeRequest({
+    url: `${baseURL}${filterId}/`,
+    method: 'DELETE',
+    headers: {'Content-Type': 'application/json', authorization: true},
+  })
+}
+
+export function updateTaskFilter(filterId, data) {
+  return makeRequest({
+    url: `${baseURL}${filterId}/`,
+    method: 'PATCH',
+    headers: {'Content-Type': 'application/json', authorization: true},
+    data
+  })
+}

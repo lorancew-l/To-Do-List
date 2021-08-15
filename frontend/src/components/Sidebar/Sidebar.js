9bd9894436819d9a55d67337c94f7888be35eb2d
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-08-15 12:27:43 +0500

    integrate task store into "Sidebar" component

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -2,27 +2,31 @@ import React from 'react'
 import { dailyTasksActive, clipboard, importantTaskActive } from '../../images/index'
 import DefaultFilter from './DefaultFilter'
 import CustomFiltersList from './CustomFiltersList'
+import { useTaskContext } from '../../store/TaskStore/TaskContext'
+import { observer } from 'mobx-react'
 
 
-export default function Sidebar(props) {
-  const icons = {today: dailyTasksActive, important: importantTaskActive, all: clipboard}
-  const defaultFilters = props.taskFilterList.filter(filter => filter.type in icons)
-  const customFilters = props.taskFilterList.filter(filter => !(filter.type in icons))
+function Sidebar(props) {
+  const taskStore = useTaskContext()
 
   return (
     <aside className={props.showSidebar? 'sidebar on' : 'sidebar off'}>
       <ul className="sidebar-filters-list">
         <ul className="default-filters">
-          {
-            defaultFilters.map(item => {
-              return <DefaultFilter selected={item.id === props.selectedFilter} taskCount={item.count}
-                                    icon={icons[item.type]} key={item.id} title={item.title} selectFilter={() => props.selectFilter(item.id)}/>
-            })
-          }
+          <DefaultFilter selected={taskStore.currentFilter.type === "today"} taskCount={taskStore.todayTasks.length}
+                         icon={dailyTasksActive} title={"Сегодня"}
+                         selectFilter={() => taskStore.setCurrentFilter({type: "today", id: null})}/>
+          <DefaultFilter selected={taskStore.currentFilter.type === "important"} taskCount={taskStore.importantTasks.length}
+                         icon={importantTaskActive} title={"Важно"}
+                         selectFilter={() => taskStore.setCurrentFilter({type: "important", id: null})}/>
+          <DefaultFilter selected={taskStore.currentFilter.type === "all"} taskCount={taskStore.tasks.length}
+                         icon={clipboard} title={"Все задачи"}
+                         selectFilter={() => taskStore.setCurrentFilter({type: "all", id: null})}/>
         </ul>
-        <CustomFiltersList customFilters={customFilters} updateFilterList={props.updateFilterList}
-                           selectFilter={props.selectFilter} selectedFilter={props.selectedFilter}/>
+        <CustomFiltersList/>
       </ul>
     </aside>
   )
 }
+
+export default observer(Sidebar)

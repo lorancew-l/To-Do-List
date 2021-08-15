d7efe180df4ddfd2df4218ea6f47d94822c13d03
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-08-15 12:21:17 +0500

    add store for tasks

---
+++ frontend/src/store/TaskStore/TaskStore.js
@@ -0,0 +1,91 @@
+import { isToday } from 'date-fns'
+import { makeAutoObservable } from 'mobx'
+import { getTaskList } from '../../tools/api/rest/tasks'
+import { getTaskFilterList } from '../../tools/api/rest/taskFilters'
+
+
+export default class TaskStore {
+  tasks = []
+  filters = []
+  currentFilter = {type: null, id: null}
+  tasksLoaded = false
+  filtersLoaded = false
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  fetchTasks() {
+    getTaskList()
+    .then(response => {
+      if (response.ok) {
+        return  response.json()
+      }
+    })
+    .then(tasks => {
+      this.tasks = tasks
+      this.tasksLoaded = true
+    })
+  }
+
+  fetchFilters() {
+    getTaskFilterList()
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+    })
+    .then(filters => {
+      this.filters = filters
+
+      if (!this.currentFilter.type) {
+        this.currentFilter = {type: 'today', id: null}
+        this.filtersLoaded = true
+      }
+    })
+  }
+
+  load() {
+    this.fetchTasks()
+    this.fetchFilters()
+  }
+
+  addTask(task) {
+    this.tasks.push(task)
+  }
+
+  setCurrentFilter({type='custom', id=null}) {
+    this.currentFilter = {type, id}
+  }
+
+  get initialLoadingComplete() {
+    return this.tasksLoaded && this.filtersLoaded
+  }
+
+  get todayTasks() {
+    return this.tasks.filter((task) => isToday(new Date(task.deadline)))
+  }
+
+  get importantTasks() {
+    return this.tasks.filter((task) => task.is_important)
+  }
+
+  get filteredTasks() {
+    if (!this.currentFilter.type) {
+      return []
+    }
+    
+    switch (this.currentFilter.type) {
+      case 'custom':
+        return this.tasks.filter((task) => task.task_filter === this.currentFilter.id)
+      case 'all':
+        return this.tasks
+      case 'today':
+        return this.todayTasks
+      case 'important':
+        return this.importantTasks
+      default:
+        return this.tasks
+    }
+  }
+}

cd3da260674b0848a434b88841fc4f2e6d07feb2
lorancew-l <71059409+lorancew-l@users.noreply.github.com>
2021-08-15 12:27:36 +0500

    integrate task store into "Homepage" component

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -3,53 +3,37 @@ import Header from './Header'
 import Sidebar from './Sidebar/Sidebar'
 import Main from './Main/Main'
 import LoadingScreen from './LoadingScreen'
-import useFetch from '../hooks/useFetch'
-import { getTaskList } from '../tools/api/rest/tasks'
-import { getTaskFilterList } from '../tools/api/rest/taskFilters'
 import { AnimatePresence, motion } from 'framer-motion'
+import { useTaskContext } from '../store/TaskStore/TaskContext'
 
 
 export default function Homepage(props) {
   const [isSidebarOpen, setSidebarOpen] = useState(window.innerWidth >= 992 ? true : false)
-  const [taskFilterId, setTaskFilterId] = useState(null)
   const [showHomePage, setShowHomePage] = useState(false)
-
-  const taskList = useFetch(getTaskList, [taskFilterId], true)
-  const taskFilterList = useFetch(getTaskFilterList)
-  const isLoaded = taskList.isLoaded && taskFilterList.isLoaded
-
-  function updateTaskList() {
-    taskList.update()
-    taskFilterList.update()
-  }
+  const taskStore = useTaskContext()
 
   useEffect(() => {
-    if (!taskFilterId && taskFilterList.value) {
-      setTaskFilterId(taskFilterList.value[0].id)
+    setTimeout(() => setShowHomePage(true), 1000)
+  }, [taskStore.initialLoadingComplete])
+
+  useEffect(() => {
+    let isMounted = true 
+
+    if (isMounted) {
+      taskStore.load()
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [taskFilterList.value])
 
-  useEffect(() => {
-    if (!taskFilterList.isLoaded || taskFilterId === null) return
-    updateTaskList()
+    return () => isMounted = false
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [taskFilterId])  
-
-  useEffect(() => {
-    if (isLoaded){
-      setTimeout(() => setShowHomePage(true), 1000)
-    }
-  }, [isLoaded])
+  }, [])
 
   return (      
     <AnimatePresence>
       {(showHomePage) ?
         <motion.div className="app" key="app" initial={{opacity: 0}} animate={{opacity: 1}}> 
           <Header isLoggedIn={props.isLoggedIn} setLoggedIn={props.setLoggedIn} onSidebarChange={() => setSidebarOpen(!isSidebarOpen)}></Header>
-          <Sidebar showSidebar={isSidebarOpen} taskFilterList={taskFilterList.value} updateFilterList={taskFilterList.update}
-                   selectedFilter={taskFilterId}  selectFilter={setTaskFilterId} ></Sidebar>
-          <Main showSidebar={isSidebarOpen} taskList={taskList.value} updateTaskList={updateTaskList} taskFilterId={taskFilterId}/>
+          <Sidebar showSidebar={isSidebarOpen}></Sidebar>
+          <Main showSidebar={isSidebarOpen}/>
         </motion.div>
         : <LoadingScreen key="loading"/>
       }
